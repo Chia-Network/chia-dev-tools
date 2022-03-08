6cb2fa2d3995a3531b3abd9e26798142594ade2a
Matt Hauff <quexington@gmail.com>
2022-03-08 10:20:33 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ dev_dependencies = [
 
 setup(
     name="chia_dev_tools",
-    version="1.0.6",
+    version="1.0.7",
     packages=find_packages(exclude=("tests",)),
     author="Quexington",
     entry_points={
