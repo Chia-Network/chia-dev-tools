56eb82ce22fc57620157cb1624e463186f29ddb6
Jack Nelson <j.nelson@chia.net>
2022-08-30 13:57:20 -0700

    increase release number (#42)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ dev_dependencies = [
 
 setup(
     name="chia_dev_tools",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(exclude=("tests",)),
     author="Quexington",
     entry_points={
