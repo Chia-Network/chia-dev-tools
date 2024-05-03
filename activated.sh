ab792efd86e1210bc8dd1505cbb00bc902a7f47f
Kyle Altendorf <sda@fstab.net>
2024-05-03 09:30:29 -0400

    move shfmt from super-linter to pre-commit (#118)
    * remove shfmt from super-linter
    
    * Update .pre-commit-config.yaml
    
    * fixup
    
    * disable git autocrlf

--- activated.sh
+++ activated.sh
@@ -2,9 +2,12 @@
 
 set -o errexit
 
-SCRIPT_DIRECTORY=$(cd -- "$(dirname -- "$0")"; pwd)
+SCRIPT_DIRECTORY=$(
+  cd -- "$(dirname -- "$0")"
+  pwd
+)
 # shellcheck disable=SC1091
 if [ -d "${SCRIPT_DIRECTORY}/venv" ]; then # CI does not use venv
-source "${SCRIPT_DIRECTORY}/venv/bin/activate"
+  source "${SCRIPT_DIRECTORY}/venv/bin/activate"
 fi
 "$@"
