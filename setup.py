cfab52e7a494dac488c5ca80d2af381acb5e5602
Geoff Walmsley <geoffwalmsley@gmail.com>
2024-10-18 14:40:17 +0300

    Update to macos-13 runners and remove python 3.8

--- setup.py
+++ setup.py
@@ -52,8 +52,9 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Security :: Cryptography",
     ],
