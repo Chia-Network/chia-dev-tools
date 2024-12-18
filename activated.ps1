4312596c433b8ee4e8dc15b755d74dcc8ebbecd0
Earle Lowe <e.lowe@chia.net>
2024-12-18 14:17:03 -0800

    update ps1 script

--- activated.ps1
+++ activated.ps1
@@ -1,9 +1,11 @@
 $ErrorActionPreference = "Stop"
 
 $script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
-if (Test-Path -Path $script_directory/venv) {
-    & $script_directory/venv/Scripts/Activate.ps1
-   }
-& @args
+$command = $args[0]
+$parameters = [System.Collections.ArrayList]$args
+$parameters.RemoveAt(0)
+
+& $script_directory/venv/Scripts/Activate.ps1
+& $command @parameters
 
 exit $LASTEXITCODE
